6c61fd2676578e109f25912b86e28df38044a7e2
Faraz Arshad <faraz.arshad125@gmail.com>
2025-07-22 16:27:36 +0500

    create students model

---
+++ API/routes/students.model.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const studentSchema = new mongoose.Schema({
+  first_name: {
+    type: String,
+    require: true,
+  },
+  last_name: {
+    type: String,
+    require: true,
+  },
+  email: {
+    type: String,
+    require: true,
+    unique: true,
+  },
+  phone: {
+    type: String,
+    require: true,
+    unique: true,
+  },
+  gender: {
+    type: String,
+    enum: ["Male", "Female", "Others"],
+  },
+  profile_pic: {
+    type: String,
+  },
+});
+
+const Student = mongoose.model('Student',studentSchema)
+modeule.exports = Student
